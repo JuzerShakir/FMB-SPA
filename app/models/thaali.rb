b765c67e0fa0703026bc2a5d8e96f4ca5b019abd
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-10-21 19:09:56 +0530

    set basic validation for thaali

--- app/models/thaali.rb
+++ app/models/thaali.rb
@@ -1,3 +1,7 @@
 class Thaali < ApplicationRecord
     enum :size, { small: 0, medium: 1, large: 2 }
+
+    validates_presence_of :number, :owner, :size
+
+    validates :number, uniqueness: true
 end
