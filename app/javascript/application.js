4c881f54fbfbfd81016d38e32f35090f2fedc3aa
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-10-22 22:25:13 +0530

    a little logic on hiding the edit and delete button when edit form is active

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,19 @@
 import "@hotwired/turbo-rails";
 import "controllers";
 import "bootstrap";
+
+// const number_field = document.getElementById("number");
+// const number_field_classes = number_field.childNodes[1].classList;
+// const create_thaali_button = document.querySelector(".create-thaali");
+
+// create_thaali_button.addEventListener("click", () => {
+//   const error = document.getElementsByClassName("error");
+//   //   console.log(error.previousElementSibling);
+//   console.log(error.item(0));
+// });
+
+// const edit_button = document.getElementById("edit-button");
+// const destroy_button = document.getElementById("destroy-button");
+// const cancel_button = document.getElementById("cancel-button");
+
+// console.log(edit_button);
