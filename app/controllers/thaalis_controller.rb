4a862473d6797a4319fde0686d89a97e6455d23d
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-10-22 16:32:01 +0530

    able to create new thaali and show them in the first row of the table using turbo and show error messages for invalid objects

--- app/controllers/thaalis_controller.rb
+++ app/controllers/thaalis_controller.rb
@@ -1,5 +1,30 @@
 class ThaalisController < ApplicationController
     def index
         @thaalis = Thaali.all
+        @thaali = Thaali.new
     end
+
+    def new
+        @thaali = Thaali.new
+    end
+
+    def create
+        @thaali = Thaali.new(thaali_params)
+
+        if @thaali.save
+            render turbo_stream: [
+                turbo_stream.prepend("thaalis", @thaali ),
+                turbo_stream.replace("new_thaali", partial: "thaalis/form")
+            ]
+        else
+            render turbo_stream: [
+                turbo_stream.replace(@thaali, partial: "thaalis/form", locals: { thaali: @thaali } )
+            ]
+        end
+    end
+
+    private
+        def thaali_params
+            params.require(:thaali).permit(:number, :owner, :size)
+        end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def inline_error_for(field, form_obj)
+        html = []
+        if form_obj.errors[field].any?
+            html << form_obj.errors[field].map do | msg |
+                tag.small(msg, class: "text-danger")
+            end
+        end
+        html.join.html_safe
+    end
 end
