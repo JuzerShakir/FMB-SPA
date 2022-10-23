3d259c85471740d90382a85616f34c6c2b938b3a
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-10-23 18:12:44 +0530

    show live validation of user input on new form

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,10 +3,6 @@ import "@hotwired/turbo-rails";
 import "controllers";
 import "bootstrap";
 
-// const number_field = document.getElementById("number");
-// const number_field_classes = number_field.childNodes[1].classList;
-// const create_thaali_button = document.querySelector(".create-thaali");
-
 // create_thaali_button.addEventListener("click", () => {
 //   const error = document.getElementsByClassName("error");
 //   //   console.log(error.previousElementSibling);
@@ -18,3 +14,49 @@ import "bootstrap";
 // const cancel_button = document.getElementById("cancel-button");
 
 // console.log(edit_button);
+
+// ? Validate form field
+
+// * Mark Valid for number input in Number field
+const number_field = document.getElementById("number_field");
+const number_rgx = /^\d+$/;
+
+number_field.addEventListener("keyup", () => {
+  let number_field_value = number_field.value;
+  let number_field_classes = number_field.classList;
+
+  // console.log(number_rgx.test(number_field_value));
+
+  if (number_rgx.test(number_field_value)) {
+    number_field_classes.add("is-valid");
+  } else {
+    number_field_classes.remove("is-valid");
+  }
+  // console.log(number_field_value);
+});
+
+// * Mark Valid or Invalid for string input in Owner field
+
+const owner_field = document.getElementById("owner_field");
+const owner_rgx = /^[a-z .]+$/i;
+
+owner_field.addEventListener("keyup", () => {
+  let owner_field_value = owner_field.value;
+  let owner_field_classes = owner_field.classList;
+
+  //   console.log(owner_rgx.test(owner_field_value));
+
+  if (owner_field_value) {
+    if (owner_rgx.test(owner_field_value)) {
+      owner_field_classes.remove("is-invalid");
+      owner_field_classes.add("is-valid");
+    } else {
+      owner_field_classes.remove("is-valid");
+      owner_field_classes.add("is-invalid");
+    }
+  } else {
+    owner_field_classes.remove("is-valid", "is-invalid");
+  }
+
+  // console.log(owner_field_value);
+});
